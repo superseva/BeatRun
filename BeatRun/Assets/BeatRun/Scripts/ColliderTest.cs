92738682eb37d4f71cd3147cd78948701e8f9d34
superseva <superseva@gmail.com>
2018-05-10 19:42:34 -0500

    test speed

---
+++ BeatRun/Assets/BeatRun/Scripts/ColliderTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderTest : MonoBehaviour {
+
+    
+	public AudioSource[] audioSources;
+	public AudioClip clip1;
+	public AudioClip clip2;
+	public AudioClip clip3;
+
+	void Start () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		//Debug.Log("ARGH");
+		if(other.name=="tik")
+		    audioSources[0].PlayOneShot(clip1);
+		else if(other.name == "tak")
+			audioSources[0].PlayOneShot(clip2);
+		else if (other.name == "tok")
+            audioSources[0].PlayOneShot(clip3);
+	}
+	private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log("TAS");
+		//audioSources[1].PlayOneShot(clip2);
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ BeatRun/Assets/BeatRun/Scripts/GameManagerTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerTest : MonoBehaviour {
+
+
+	public GameObject myQuad;
+    
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject go;
+		for (int i = 0; i < 30;i++){
+			Vector3 poz = new Vector3(i*2, 0, 0);
+			go = Instantiate(myQuad, poz, Quaternion.identity);
+			go.transform.parent = this.gameObject.transform;
+			go.name = "tik";
+		}
+
+		for (int n = 0; n < 30; n++)
+        {
+			Vector3 poz = new Vector3((n * 2)+1, 0, 0);
+            go = Instantiate(myQuad, poz, Quaternion.identity);
+            go.transform.parent = this.gameObject.transform;
+			go.GetComponent<Renderer>().material.color = Color.green;
+            go.name = "tak";
+        }
+
+		for (int t = 0; t < 15; t++)
+        {
+            Vector3 poz = new Vector3((t * 4), 0, 0);
+            go = Instantiate(myQuad, poz, Quaternion.identity);
+            go.transform.parent = this.gameObject.transform;
+            go.GetComponent<Renderer>().material.color = Color.blue;
+            go.name = "tok";
+        }
+	}
+    
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate(Vector3.left * Time.deltaTime *2.0f);
+	}
+}
